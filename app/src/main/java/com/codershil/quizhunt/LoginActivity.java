eb08044abf5aee12eb3a01ee3d2de9de554016a4
shil-alone <shilalone007@gmail.com>
2021-04-17 19:56:55 +0530

    added sigup and sign in functionality using firebase authentication and saved user data using firestore

--- app/src/main/java/com/codershil/quizhunt/LoginActivity.java
+++ app/src/main/java/com/codershil/quizhunt/LoginActivity.java
@@ -1,14 +1,72 @@
 package com.codershil.quizhunt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.codershil.quizhunt.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.zip.DeflaterInputStream;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding ;
+    FirebaseAuth auth ;
+    ProgressDialog dialog ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("logging in...");
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String email, pass;
+                email = binding.emailBox.getText().toString();
+                pass = binding.passwordBox.getText().toString();
+                dialog.show();
+
+                auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            dialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                            finish();
+                        }
+                        else{
+                            dialog.dismiss();
+                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/codershil/quizhunt/SignUpActivity.java
+++ app/src/main/java/com/codershil/quizhunt/SignUpActivity.java
@@ -1,14 +1,86 @@
 package com.codershil.quizhunt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.codershil.quizhunt.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding ;
+    FirebaseAuth auth ;
+    FirebaseFirestore database ;
+    ProgressDialog dialog ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("We're creating a new account");
+
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email ,pass , name , referCode;
+                email = binding.emailBox.getText().toString();
+                pass = binding.passwordBox.getText().toString();
+                name = binding.nameBox.getText().toString();
+                referCode = binding.nameBox.getText().toString();
+
+                final User user = new User(name ,email,pass,referCode);
+
+                dialog.show();
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            dialog.dismiss();
+                            String uid = task.getResult().getUser().getUid();
+                            database.collection("users:")
+                                    .document(uid)
+                                    .set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        dialog.dismiss();
+                                        startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+                                        finish();
+                                    }
+                                    else {
+                                        Toast.makeText(SignUpActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                            Toast.makeText(SignUpActivity.this, "success", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else {
+                            dialog.dismiss();
+                            Toast.makeText(SignUpActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
