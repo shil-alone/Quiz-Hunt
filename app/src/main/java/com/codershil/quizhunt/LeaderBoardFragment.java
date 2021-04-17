4f3a0c83bb45b658eb38c4a49450e1760bc28f84
shil-alone <shilalone007@gmail.com>
2021-04-17 15:19:16 +0530

    designed leaderboard fragment and its item view for recycler view

---
+++ app/src/main/java/com/codershil/quizhunt/LeaderBoardFragment.java
@@ -0,0 +1,30 @@
+package com.codershil.quizhunt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeaderBoardFragment extends Fragment {
+
+
+    public LeaderBoardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+    }
+}
