5a23e046f07156958d1d436d0b57dc6c5c6cd6e7
shil-alone <shilalone007@gmail.com>
2021-04-23 18:14:20 +0530

    updated coins in firebase firestore when users send withdraw reqeust

--- app/src/main/java/com/codershil/quizhunt/WalletFragment.java
+++ app/src/main/java/com/codershil/quizhunt/WalletFragment.java
@@ -1,8 +1,11 @@
 package com.codershil.quizhunt;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +13,11 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.codershil.quizhunt.databinding.FragmentWalletBinding;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class WalletFragment extends Fragment {
@@ -31,12 +36,17 @@ public class WalletFragment extends Fragment {
     FragmentWalletBinding binding ;
     FirebaseFirestore database;
     User user ;
+    String uid;
+    long coins ;
+    ProgressDialog dialog ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentWalletBinding.inflate(inflater,container,false);
+        dialog = new ProgressDialog(getContext());
+        dialog.setMessage("sending withdraw request...");
 
         database = FirebaseFirestore.getInstance();
         database.collection("users")
@@ -45,6 +55,7 @@ public class WalletFragment extends Fragment {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 user = documentSnapshot.toObject(User.class);
+                coins = user.getCoins();
                 binding.txtCollectedCoins.setText(String.valueOf(user.getCoins()));
             }
         });
@@ -52,12 +63,13 @@ public class WalletFragment extends Fragment {
         binding.btnSendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                dialog.show();
                 if(user.getCoins() >= 50000){
-                    String uid =FirebaseAuth.getInstance().getUid();
+                    uid =FirebaseAuth.getInstance().getUid();
                     String paypalEmail = binding.edtPaypalEmail.getText().toString();
                     if (paypalEmail.isEmpty()){
                         binding.edtPaypalEmail.setError("please enter email first");
+                        dialog.dismiss();
                         return;
                     }
                     WithdrawRequest request = new WithdrawRequest(uid,paypalEmail,user.getName());
@@ -68,11 +80,30 @@ public class WalletFragment extends Fragment {
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            Toast.makeText(getContext(), "Request sent successfully", Toast.LENGTH_SHORT).show();
+                            database.collection("users")
+                                    .document(uid)
+                                    .update("coins", FieldValue.increment(-50000))
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    binding.txtCollectedCoins.setText(String.valueOf(coins-50000));
+                                    Toast.makeText(getContext(), "wallet coins updated", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            dialog.dismiss();
+                            Toast.makeText(getContext(), "Request sent successfully .you will receive your reward within 48 hours", Toast.LENGTH_LONG).show();
                         }
                     })
-                    ;
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            dialog.dismiss();
+                            Toast.makeText(getContext(), "unable to send request , try again!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
+
+
                 else{
                     Toast.makeText(getContext(), "You Need More coins for withdrawal", Toast.LENGTH_SHORT).show();
                 }
