501e2d67f16d461676594dbf61248ba7b28d954c
shil-alone <shilalone007@gmail.com>
2021-05-07 13:28:15 +0530

    added profile editing feature

--- app/src/main/java/com/codershil/quizhunt/ProfileFragment.java
+++ app/src/main/java/com/codershil/quizhunt/ProfileFragment.java
@@ -2,15 +2,34 @@ package com.codershil.quizhunt;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.codershil.quizhunt.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class ProfileFragment extends Fragment {
-
+    FragmentProfileBinding binding ;
+    FirebaseAuth auth ;
+    FirebaseUser user ;
+    FirebaseFirestore database;
+    private String password ;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -24,7 +43,53 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater,container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseFirestore.getInstance();
+
+        database.collection("users")
+                .document(user.getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        User userName = documentSnapshot.toObject(User.class);
+                        binding.edtName.setText(userName.getName());
+                    }
+                });
+
+        binding.btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.edtName.getText().toString();
+
+                if (name.isEmpty()){
+                    binding.edtName.setError("please enter name first");
+                }
+
+                if (!name.isEmpty()){
+                    database.collection("users")
+                            .document(user.getUid())
+                            .update("name",name)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(getContext(), "profile updated successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), "unable to update the name", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+
+            }
+        });
+
+        return binding.getRoot();
     }
 }
